f907feee04772361a5b5005efc026886d49c04cc
nadaletomas <157069712+nadaletomas@users.noreply.github.com>
2025-03-16 19:44:57 -0300

    Create forestData.js

---
+++ forestData.js
@@ -0,0 +1,26 @@
+let cityName = "Techville";
+let parkCount = 5;
+let parks = [
+    {
+        name: "Central Park",
+        trees: [
+            { species: "Maple", age: 5, health: "Good", height: 20 },
+            { species: "Birch", age: 7, health: "Good", height: 18 }
+        ],
+        facilities: ["playground", "swimming pool"]
+    },
+    {
+        name: "Riverside Park",
+        trees: [
+            { species: "Oak", age: 30, health: "Excellent", height: 25 },
+            { species: "Redwood", age: 50, health: "Good", height: 100 }
+        ],
+        facilities: ["boating", "fishing"]
+    },
+    {
+        name: "Hilltop Park",
+        trees: [
+            { species: "Pine", age: 10, health: "Fair", height: 15 }
+        ],
+        facilities: ["hiking trails"]
+    }];
